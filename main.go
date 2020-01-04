cb576f7c78399b4cd74b83a02ccd899945a78796
TCoXXV <tccodingxxv@gmail.com>
2020-01-04 01:49:26 -0700

    progress

--- main.go
+++ main.go
@@ -2,13 +2,42 @@ package main
 
 import (
 	"fmt"
-	// "net/http"
+	"log"
+	"net/http"
+	"html/template"
 )
 
-func main() {
-	fmt.Println("test 2")
+func servHome(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	rendered(w, "home.gohtml", map[string]int{"path": 1,})
+}
+
+func servTest(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	rendered(w, "index.gohtml", map[string]int{"path": 2,})
+}
+
+func rendered(w http.ResponseWriter, f string, v map[string]int) {
+	tpl.ExecuteTemplate(w, f, v)
+	// writer, file, v
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("views/*"))
 }
 
 func main() {
-	fmt.Println("test 2")
+	http.HandleFunc("/", servHome)
+	http.HandleFunc("/test", servTest)
+
+	fmt.Println("Listening on port 1337")
+	http.ListenAndServe(":1337", nil)
 }
